506553977aea72230ada895d4d90748a12ca8601
Palak Mishra <mishrapalak2410@gmail.com>
2026-09-06 22:10:06 +0530

    Added function_with_returnvalue  practice

---
+++ js/function_with_returnvalue.js
@@ -0,0 +1,17 @@
+//function with return value 
+function sum(a,b){
+    var s=a+b;
+    return s;
+}
+
+var s=sum(10.4,23.4);
+console.log(s);
+
+
+function fullName(fname="PALAK",lname="MISHRA"){
+    var a=fname + " " + lname;
+    return a;
+}
+
+var n=fullName();
+console.log(n);
