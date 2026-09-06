d7acb569fd8452ea0c228803d6393d3ac0ef397a
Palak Mishra <mishrapalak2410@gmail.com>
2026-09-06 21:36:10 +0530

    Added alert box practice

---
+++ js/alertbox.js
@@ -0,0 +1,15 @@
+alert("HELLO EVERYONE");
+//pop-up message
+
+//with condition 
+var a=10;
+var b=20;
+if (a>b){
+    alert(a + " is greater");
+}
+else{
+    alert(b + "is greater");
+}
+
+//arithmetic opeartion 
+alert(a+b);
