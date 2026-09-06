b87363bc5bdd0d3b38c20ab9f8dc9ab6ef8f8302
Palak Mishra <mishrapalak2410@gmail.com>
2026-09-06 21:41:29 +0530

    Added confirm box practice

---
+++ js/confirmbox.js
@@ -0,0 +1,13 @@
+//confirm box
+confirm("Do you like our website?");
+//ask for ok or cancel 
+
+var a=confirm("Do you like our website?");
+alert(a);
+
+if(a){
+    alert("THANKS");
+}
+else{
+    alert("SORRY");
+}
