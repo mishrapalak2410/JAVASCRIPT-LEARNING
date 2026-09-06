38293ad0bff1561714ba183e5cc8e3fa45fd1e70
Palak Mishra <mishrapalak2410@gmail.com>
2026-09-06 21:53:41 +0530

    Added function practice

---
+++ js/function.js
@@ -0,0 +1,12 @@
+ //function 
+//reusability and reduce redundancy
+// function functionName(){statement(s)}
+
+function hello(){
+    console.log("hello !!!");
+}
+hello();
+console.log("<br>");
+hello();
+hello();
+
