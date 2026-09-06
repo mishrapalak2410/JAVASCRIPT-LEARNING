a323912da1a7ceafaf96650e607628123ef3113c
Palak Mishra <mishrapalak2410@gmail.com>
2026-09-06 22:32:28 +0530

    Added EVENTS  practice

---
+++ js/Events.js
@@ -0,0 +1,23 @@
+//basic events
+/* 
+1-click ---onclick
+2-double click ---ondblclick
+3-right click  ---oncontextmenu
+4-mouse hover ----onmouseenter
+5-mouse out    ---onmouseout
+6-mouse down    ---onmousedown
+7-mouse up     ---onmouseup
+8-key press    ---onkeypress {body tag, form tag}
+9-key up      ---onkeyup
+10-load       ---onload {body tag}
+11-unload    ---onunload {body tag}
+12-resize     ---onresize {body tag}
+13-scroll    ---onscroll  
+*/
+
+function hello(){
+    console.log("hello");
+}
+
+//<button onclick="hello()">click me</button>
+ 
